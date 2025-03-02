1d1436c45f822d7758a4bb63b576fc102fa757ea
Thư <84165791+thumaigit@users.noreply.github.com>
2025-03-02 07:22:15 +0000

    nx init: create server and client

---
+++ nestjs/jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'nestjs',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/nestjs',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
